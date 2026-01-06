6bee1df78fb936bdba6bd032bdecb688ab0524cf
andrew <git@thewilsonsdomain.com>
2026-01-06 11:57:42 -0600

    Completed Course 5 - Chapter 3

---
+++ Course05/CH03L04.py
@@ -0,0 +1,18 @@
+class Wizard:
+    def __init__(self, name, stamina, intelligence):
+        self.name = name
+        self.__stamina = stamina
+        self.__intelligence = intelligence
+        self.mana = self.__intelligence * 10
+        self.health = self.__stamina * 100
+
+    # don't touch above this line
+
+    def get_fireballed(self, fireball_damage):
+        fireball_damage -= self.__stamina
+        self.health -= fireball_damage
+
+    def drink_mana_potion(self, potion_mana):
+        potion_mana += self.__intelligence
+        self.mana += potion_mana
+
