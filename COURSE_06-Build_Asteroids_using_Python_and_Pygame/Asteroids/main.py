1e5409f41e9200c0efe685392e5cb03820005c94
Andrew Wilson <github@thewilsonsdomain.com>
2026-02-25 17:15:39 -0600

    COURSE_06 Added clock

--- COURSE_06-Build_Asteroids_using_Python_and_Pygame/Asteroids/main.py
+++ COURSE_06-Build_Asteroids_using_Python_and_Pygame/Asteroids/main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 
 
